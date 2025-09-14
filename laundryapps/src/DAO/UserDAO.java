2d7bfc9bee0c6722eee36d91924b1054e19fee34
DD-Bin <ddbintang19g@gmail.com>
2025-09-14 23:05:15 +0700

    commit

--- laundryapps/src/DAO/UserDAO.java
+++ laundryapps/src/DAO/UserDAO.java
@@ -1,5 +1,15 @@
 package DAO;
 
+import java.util.List;
+
+import javax.swing.JList;
+
+import model.User;
+
 public interface UserDAO {
+	void save(User user);
+	public List<User> show();
+	public void delete(String id);
+	public void update(User user); 
 
 }
