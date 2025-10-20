3be1c3117797957a746b7d7f21a695834e78177b
DD-Bin <ddbintang19g@gmail.com>
2025-10-20 14:22:42 +0700

    pekan4

--- Praktikum_PBO/src/pekan4/book.java
+++ Praktikum_PBO/src/pekan4/book.java
@@ -1,5 +1,21 @@
 package pekan4;
 
 public class book {
+	private String title;
+	private String author;
+	private boolean isAvailable;
+	
+	public book ( String title, String author) {
+		this.title = title;
+		this.author= author;
+		this.isAvailable = true;
+	}
+	
+	public String getTitle() { return title; }
+	public String getAuthor() { return author; }
+	public boolean isAvailable() { return isAvailable; }
+	
+	public void borrowBook() { this.isAvailable = false; }
+	public void returnBook() { this.isAvailable = true; }
 
 }
